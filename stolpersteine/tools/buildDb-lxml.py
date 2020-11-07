837533bf29315192839a91320e44361729865290
Josef Schugt <penpendede@mail.ru>
2020-11-08 00:34:57 +0100

    generate sqlite database: three versions + README

---
+++ stolpersteine/tools/buildDb-lxml.py
@@ -0,0 +1,75 @@
+from lxml import etree
+import sqlite3
+import murmurhash.mrmr
+import ctypes
+
+def gethash(value):
+    return "{0:08x}".format(ctypes.c_ulong(murmurhash.mrmr.hash(value)).value)
+
+def gethex(value):
+    return "{0:09x}".format(value)
+
+def getid(id, k, v):
+    return gethex(id) + gethash(k) + gethash(v) 
+
+fileNames = [
+    './stolperstein-world/stolperstein-europe.osm',
+    './stolperstein-world/stolperstein-russia.osm'
+]
+
+conn = sqlite3.connect('./stolpersteine/db.sqlite3')
+c = conn.cursor()
+
+c.execute('PRAGMA JOURNAL_MODE=OFF')
+c.execute('PRAGMA SYNCHRONOUS=OFF')
+c.execute('PRAGMA LOCKING_MODE=EXCLUSIVE')
+c.execute('PRAGMA TEMP_STORE=MEMORY')
+c.execute('DROP TABLE IF EXISTS stein')
+c.execute('DROP TABLE IF EXISTS kv')
+c.execute('''
+    CREATE TABLE stein (
+        id INTEGER NOT NULL PRIMARY KEY,
+        geometry TEXT,
+        version INTEGER,
+        timestamp TEXT
+    )''')
+c.execute('''
+    CREATE TABLE kv (
+        hash TEXT PRIMARY KEY,
+        nodeid INTEGER,
+        key TEXT,
+        value TEXT,
+        FOREIGN KEY(nodeid) REFERENCES stein(nodeid)
+    )''')
+
+steinData = []
+kvData = []
+for fileName in fileNames:
+    tree = etree.parse(fileName)
+    root = tree.getroot()
+    for child in root:
+        if child.tag == 'node':
+            steinData.append(
+                (
+                    int(child.attrib['id']),
+                    '{type: "Point",coordinates:[' + child.attrib['lon'] + ',' + child.attrib['lat'] + ']}',
+                    int(child.attrib['version']),
+                    child.attrib['timestamp']
+                )
+            )
+            for grandchild in child:
+                if grandchild.tag == 'tag':
+                    k = grandchild.attrib['k']
+                    v = grandchild.attrib['v']
+                    if k:
+                        kvData.append(
+                            (
+                                getid (int(child.attrib['id']), k, v),
+                                int(child.attrib['id']),
+                                k,
+                                v
+                            )
+                        )
+c.executemany('INSERT OR REPLACE INTO stein VALUES (?,?,?,?)', steinData)
+c.executemany('INSERT OR REPLACE INTO kv VALUES (?,?,?,?)', kvData)
+conn.commit()
--- stolpersteine/tools/buildDb.js
+++ stolpersteine/tools/buildDb.js
@@ -1,42 +1,101 @@
 const convert = require('xml-js')
+const sqlite = require('sqlite3')
+const hash = require('imurmurhash')
 const fs = require('fs')
 
 const requiredAttributes = ['id', 'lat', 'lon', 'version', 'timestamp']
+const fileNames = [
+  'stolperstein-world/stolperstein-europe.osm',
+  'stolperstein-world/stolperstein-russia.osm'
+]
 
-fs.readFile('stolperstein-world/stolperstein-russia.osm', 'utf8', (err, xml) => {
-  if (!err) {
-    const data = JSON.parse(
-      convert.xml2json(xml, {
-        compact: true
-      })
-    )
-    if (data && data.osm && data.osm.node) {
-      const nodes = data.osm.node
-      nodes.forEach(node => {
-        const attributes = node._attributes
-        var okay = true
-        if (attributes) {
-          requiredAttributes.forEach(attr => {
-            okay = okay && Object.prototype.hasOwnProperty.call(attributes, attr)
-          })
-        }
-        if (okay) {
-          var id = attributes.id
-          console.log(id, attributes.lat, attributes.lon, attributes.version, attributes.timestamp)
-          const tags = node.tag
-          if (tags) {
-            tags.forEach(tag => {
-              if (tag && tag._attributes) {
-                const key = tag._attributes.k
-                const value = tag._attributes.v
-                if (key) {
-                  console.log(id, key, value)
-                }
+function getHash (value) {
+  return ('00000000' + hash(value).result().toString(16)).slice(-8)
+}
+function getHex (value) {
+  return ('000000000' + parseInt(value).toString(16)).slice(-9)
+}
+function getId (id, k, v) {
+  return getHex(id) + getHash(k) + getHash(v)
+}
+
+const db = new sqlite.Database('./stolpersteine/db.sqlite3', function withTable (error) {
+  if (!error) {
+    db.serialize(function withSerialized (error) {
+      if (!error) {
+        db.run('PRAGMA JOURNAL_MODE=OFF')
+          .run('PRAGMA SYNCHRONOUS=OFF')
+          .run('PRAGMA LOCKING_MODE=EXCLUSIVE')
+          .run('PRAGMA TEMP_STORE=MEMORY')
+          .run('DROP TABLE IF EXISTS stein')
+          .run('DROP TABLE IF EXISTS kv')
+          .run(
+            'CREATE TABLE stein (' +
+            'id INTEGER NOT NULL PRIMARY KEY,' +
+            'geometry TEXT,' +
+            'version INTEGER,' +
+            'timestamp TEXT' +
+            ')'
+          )
+          .run(
+            'CREATE TABLE kv (' +
+            'hash TEXT PRIMARY KEY,' +
+            'nodeid INTEGER,' +
+            'key TEXT,' +
+            'value TEXT,' +
+            'FOREIGN KEY(nodeid) REFERENCES stein(nodeid)' +
+            ')'
+          )
+        var insertStein = db.prepare('INSERT OR REPLACE INTO stein VALUES (?, ?, ?, ?)')
+        var insertKv = db.prepare('INSERT OR REPLACE INTO kv VALUES (?, ?, ?, ?)')
+        fileNames.forEach(function fileLoop (fileName) {
+          let data
+          fs.readFile(fileName, 'utf8', (err, xml) => {
+            if (!err) {
+              data = JSON.parse(
+                convert.xml2json(xml, {
+                  compact: true
+                })
+              )
+              if (data && data.osm && data.osm.node) {
+                const nodes = data.osm.node
+                nodes.forEach(node => {
+                  const attributes = node._attributes
+                  var okay = true
+                  if (attributes) {
+                    requiredAttributes.forEach(attr => {
+                      okay = okay && Object.prototype.hasOwnProperty.call(attributes, attr)
+                    })
+                  }
+                  if (okay) {
+                    var id = attributes.id
+                    insertStein.run(
+                      id,
+                      '{type: "Point",coordinates:[' + attributes.lon + ',' + attributes.lat + ']}',
+                      attributes.version,
+                      attributes.timestamp
+                    )
+                    const tags = node.tag
+                    if (tags) {
+                      let k
+                      let v
+                      tags.forEach(tag => {
+                        if (tag && tag._attributes) {
+                          k = tag._attributes.k
+                          v = tag._attributes.v
+                          if (k) {
+                            insertKv.run(getId(id, k, v), id, k, v)
+                          }
+                        }
+                      })
+                    }
+                  }
+                })
               }
-            })
-          }
-        }
-      })
-    }
+            }
+          })
+        })
+      }
+    })
   }
 })
