0cd1bcac59138ecdc5bb450072eb61b8d0f9f55e
Josef Schugt <penpendede@mail.ru>
2020-11-10 23:11:38 +0100

    make simple geoJSON

--- stolpersteine/tools/makeGeoJSON.js
+++ stolpersteine/tools/makeGeoJSON.js
@@ -1,9 +1,17 @@
+const fs = require('fs')
 const sqlite = require('sqlite3')
+
 const db = new sqlite.Database('./stolpersteine/db.sqlite3', function withTable (error) {
   if (!error) {
     db.all('select geometry from stein', [], function processSteins (error, rows) {
       if (!error) {
-        console.log(rows)
+        fs.writeFileSync('./stolpersteine/all.geojson',
+          '{"type":"FeatureCollection","features":[' +
+          rows.map(function (row) {
+            return '{"type":"Feature","geometry":' + row.geometry.replace(/ /g, '') + ',"properties":{}}'
+          }).join() +
+          ']}'
+        )
       }
     })
   }
